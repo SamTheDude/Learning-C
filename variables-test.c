0bed2fa896525406428e1fa0062a2dbb9bf5a5e9
samkent <kent_sam@outlook.com>
2020-01-14 13:18:07 +0000

    Add variable test c code.

---
+++ variables-test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main()
+{
+    int simpleNum = 64; 	// you must always
+    float decimal = 76.4;	// declare variables
+    char letter = ‘Z‘;	// before using them
+
+    printf(”The integer is %i\n”, simpleNum);
+    printf(”The decimal is %f\n”, decimal);
+    printf(”The character is %c\n”, letter);
+    printf(”Life, the universe & everything: %i\n”, 42);
+}
