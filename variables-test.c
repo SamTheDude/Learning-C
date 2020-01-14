26930b24f32ea4ad291d5f8abcba73cd64d5b2ca
Samuel Kent <kent_sam@outlook.com>
2020-01-14 14:05:36 +0000

    Done another few tests using inputs and outputs.

--- variables-test.c
+++ variables-test.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 main()
 {
-    int simpleNum = 64; 	// you must always
-    float decimal = 76.4;	// declare variables
-    char letter = ‘Z‘;	// before using them
+    //Variable declarations.
+    int simpleNum = 64;
+    float decimal = 76.4;
+    char letter = 'Z';
 
-    printf(”The integer is %i\n”, simpleNum);
-    printf(”The decimal is %f\n”, decimal);
-    printf(”The character is %c\n”, letter);
-    printf(”Life, the universe & everything: %i\n”, 42);
+    //Test input of an integer.
+    printf("Enter a value for the integer: ");
+	scanf("\n%i",&simpleNum); 
+
+    printf("The integer is %i\n", simpleNum);
+    printf("The decimal is %f\n", decimal);
+    printf("The character is %c\n", letter);
+    printf("Life, the universe & everything: %i\n", 42);
 }
